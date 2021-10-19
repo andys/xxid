ca47c03846d94c6e3e8c4913cc0511e31e91f0cc
Andrew Snow <andrew@modulus.org>
2021-10-19 15:04:00 +1100

    updated

--- hostid_linux.go
+++ hostid_linux.go
@@ -5,6 +5,11 @@ package xxid
 import "io/ioutil"
 
 func readPlatformMachineID() (string, error) {
-	b, err := ioutil.ReadFile("/sys/class/dmi/id/product_uuid")
+	b, err := ioutil.ReadFile("/var/lib/dbus/machine")
+	if err != nil {
+		// try fallback path
+		b, err = readFile("/etc/machine-id")
+	}
+
 	return string(b), err
 }
